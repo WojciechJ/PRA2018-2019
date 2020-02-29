fb7af275e54321ded887c63d5ab45cc6cc7197c0
Jagodziński Wojciech <w.jagodzinski.24@gmail.com>
2020-02-29 10:09:52 +0100

    logger

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,20 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    final static Logger log;
+
+    static {
+        log = Logger.getLogger(HelloWorld.class);
+    }
+
     public static void main(String [ ] args) {
+
+
         System.out.println("Hello World");
+        log.info("message");
+
     }
 }
